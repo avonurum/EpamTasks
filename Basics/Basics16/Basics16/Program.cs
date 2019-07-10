23a97bcb3603d865965a3db5b443518f0c173f5a
Anna Murunova <asmurunova@gmail.com>
2019-07-10 16:13:11 +0400

    Basics

---
+++ Basics/Basics16/Basics16/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basics16
+{
+    /*   ---Задание 1.6---
+    *   Для форматирования текста надписи можно использовать различные : полужирное, курсивное и подчёркнутое, а также их сочетания.
+    *   Предложите способ хранения информации о форматировании текста надписи и напишите программу, которая позволяет устанавливать и 
+    *   изменять начертание:*/
+    enum TypesOfStyle
+    {
+        none,
+        bold,
+        italic,
+        underline,
+        empty
+    }
+    class Program
+    {
+        static bool isContinue = true;
+        static void Main(string[] args)
+        {
+            TypesOfStyle[] f = new TypesOfStyle[4] { TypesOfStyle.none, TypesOfStyle.empty, TypesOfStyle.empty, TypesOfStyle.empty }; //массив
+
+            while (isContinue)
+            {
+                Menu(f);
+                ShowState(f);
+            }
+        }
+
+        static void Menu(TypesOfStyle[] state) //меню
+        {
+            Console.WriteLine($"Введите:\n 1.{TypesOfStyle.bold.ToString()}\n 2.{TypesOfStyle.italic.ToString()} \n 3." +
+                                $"{TypesOfStyle.underline.ToString()}+\n Exit");
+            switch (Console.ReadLine())
+            {
+                case "1": ChangeState(TypesOfStyle.bold, state); break;
+                case "2": ChangeState(TypesOfStyle.italic, state); break;
+                case "3": ChangeState(TypesOfStyle.underline, state); break;
+                case "Exit": isContinue = false; break;
+                default: Console.WriteLine("Введено неверное значение."); Menu(state); break;
+            }
+        }
+
+        static void ChangeState(TypesOfStyle c, TypesOfStyle[] state)//метод для изменения состояния
+        {
+            if (state[(int)c] == TypesOfStyle.empty)//если выбранный стиль еще не был задействован, то выбираем его
+            {
+                state[(int)c] = c;
+            }
+            else //иначе убираем данный стиль
+            {
+                state[(int)c] = TypesOfStyle.empty;
+            }
+
+            int empty = 0;//счетчик
+
+            for (int i = 1; i < state.Length; i++)
+            {
+                //если стиль равен empty увеличиваем счетчик
+                if (state[i] == TypesOfStyle.empty)
+                {
+                    empty++;
+                }
+            }
+            //если все 3 стиля(bold,italic,underline)пусты нам нужно оповестить пользователя о том, что не применен ни один стиль
+            if (empty == 3)
+            {
+                state[0] = TypesOfStyle.none;
+            }
+            else
+            {
+                state[0] = TypesOfStyle.empty;
+            }
+        }
+
+        static void ShowState(TypesOfStyle[] state)//вывод стилей
+        {
+            Console.WriteLine("Параметры надписи:");
+            if (state[0] == TypesOfStyle.none)
+            {
+                Console.Write(state[0]);
+            }
+            else
+            {
+                for (int i = 0; i < state.Length; i++)
+                {
+                    if (state[i] != TypesOfStyle.empty) Console.Write(state[i].ToString() + " ");
+                }
+            }
+            Console.Write(Environment.NewLine);
+        }
+    }
+}
---
+++ Basics/Basics16/Basics16/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Basics16")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Basics16")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("6e12db52-f742-4de2-8934-ae841f08bf4b")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
