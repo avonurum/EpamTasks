3ef210b08cd4eabdd576fb35dd6d722b18a6113d
Anna Murunova <asmurunova@gmail.com>
2019-07-10 16:06:15 +0400

    String

---
+++ String/String 12/String 12/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_12
+{
+    /*---Задание 1.12---
+     * Написать программу, которая удваивает в первой введённой строке все символы, принадлежащие второй введённой строке.
+     * */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fString = AddValue();//строка 1
+            string sSrting = AddValue();//строка 2
+            string result = toDoubleChar(ref fString,ref sSrting);
+            
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+
+        public static string AddValue()//добавление значения
+        {
+            Console.WriteLine("Введите строку:");
+            return Console.ReadLine();
+        }
+        public static string toDoubleChar(ref string fstr,ref string sstr)//удвоение символов
+        {
+            for (int i = 0; i < sstr.Length; i++)
+            { 
+                string st = sstr[i].ToString() + sstr[i].ToString();
+                string e = sstr[i].ToString();
+                fstr = fstr.Replace(e, st);
+                sstr = sstr.Replace(sstr[i],' ');//заменяем все повторяющиеся символы на пробелы
+            }
+            return fstr;
+        }
+
+    }
+}
---
+++ String/String11/String11/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String11
+{
+    class Program
+        /* ---Задание 1.11---
+         * Написать программу, которая определяет среднюю длину слова во введённой текстовой строке. Учесть, что символы пунктуации на длину слов влиять не должны. Регулярные выражения не использовать.
+         * И не пытайтесь прописать все символы-разделители ручками. Используйте стандартные методы классов String и Char.
+         * */
+    {
+        static void Main(string[] args)
+        {
+            string str = AddValue();
+            char[] list = FindSep(str);//массив разделителей и знаков пунктуации
+            string[] words = str.Split(list,StringSplitOptions.RemoveEmptyEntries);//разделение строки на слова
+            int middle = MiddleLength(ref words,ref list);
+            Console.WriteLine(middle);
+            Console.ReadKey();
+        }
+
+        public static string AddValue()//добавление строки
+        {
+            Console.WriteLine("Введите строку:");
+            return Console.ReadLine();
+        }
+
+        public static int MiddleLength(ref string[] words, ref char[]list)//нахождение средней длины слова
+        {
+            int sum = 0;
+            foreach (string w in words)
+            {
+                sum += w.Length;
+            }
+            return sum / words.Length;
+        }
+
+        public static char[] FindSep(string str)//нахождение знаков пунктуации и разделителей.
+        {
+            char[] list = new char[20];
+            int i = 0;
+            foreach (char c in str)
+            {
+
+                if (char.IsPunctuation(c) == true || char.IsSeparator(c) == true)
+                {
+                    list[i] = c;
+                    i++;
+                }
+            }
+            return list;
+        }
+    }
+}
