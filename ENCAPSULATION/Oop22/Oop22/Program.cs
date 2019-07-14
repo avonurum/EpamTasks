9f3ed78e2973300201059087db97c12c1f7cee25
Anna Murunova <asmurunova@gmail.com>
2019-07-15 01:05:31 +0400

    Задача 2.2

--- ENCAPSULATION/Oop22/Oop22/Program.cs
+++ ENCAPSULATION/Oop22/Oop22/Program.cs
@@ -6,10 +6,64 @@ using System.Threading.Tasks;
 
 namespace Oop22
 {
+    /* ---Задание 2.2---
+     * Написать класс Triangle, описывающий треугольник со сторонами a, b, c 
+     * и позволяющий осуществить расчёт его площади и периметра. Написать программу, демонстрирующую использование данного треугольника.
+     * */
     class Program
     {
+        enum TriangleParam
+        {
+            Сторона_а,
+            Сторона_b,
+            Сторона_c,
+            Периметр,
+            Площадь
+        }
+        static bool status=true;
         static void Main(string[] args)
         {
+            Triangle triangle = new Triangle(3,3,3);
+            while (status == true)
+            {
+                Menu(ref triangle);
+            }
+        }
+
+        static void Menu(ref Triangle triangle)
+        {
+            Console.WriteLine("Выберите: ");
+            Console.WriteLine(" 1.Вывести стороны треугольника \n 2.Изменить сторону a \n 3.Изменить сторону b \n " +
+                "4.Изменить сторону c \n 5.Вывести площадь треугольника \n 6.Вывести периметр треугольника \n" +
+                " 7.Выход");
+            switch (Console.ReadLine())
+            {
+                case "1": Console.WriteLine($"{TriangleParam.Сторона_а.ToString()}:{triangle.A}\n" +
+                    $"{TriangleParam.Сторона_b.ToString()}:{triangle.B}\n{TriangleParam.Сторона_c.ToString()}:{triangle.C}"); break;
+                case "2": triangle.A = AddValue(TriangleParam.Сторона_а); break;
+                case "3": triangle.B = AddValue(TriangleParam.Сторона_b); break;
+                case "4": triangle.C = AddValue(TriangleParam.Сторона_c); break;
+                case "5": Console.WriteLine(triangle.Area()); break;
+                case "6": Console.WriteLine(triangle.Perimetr()); break;
+                case "7": status = false; break;
+                default: Console.WriteLine("Вы ввели неверное значение"); break;
+            }
+        }
+
+        static double AddValue(TriangleParam param)//добавление значения сторон треугольника
+        {
+            Console.Write(param + ":");
+            string temp = Console.ReadLine();
+            try
+            {
+                return double.Parse(temp);
+
+            }
+            catch
+            {
+                Console.WriteLine("Вы ввели не число. Повторите ввод");
+                return AddValue(param);
+            }
         }
     }
 }
---
+++ ENCAPSULATION/Oop22/Oop22/Triangle.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Math;
+
+namespace Oop22
+{
+    //для того чтобы тругольник со сторонами a,b,c существовал, нужно чтобы выполнялись условия : a + b > c and a + c > b and b + c > a:
+    class Triangle
+    {
+        private double a;
+        private double b;
+        private double c;
+
+        public Triangle(double a, double b, double c)
+        {
+                A = a;
+                B = b;
+                C = c;
+        }
+
+        public double A {
+            get => a;
+            set
+            {
+                if (value > 0)
+                {
+                    if(b!=0 && c!=0 && !isTriangleExist(value,b,c))
+                    {
+                        throw new ArgumentException("Треугольник с данными длинами сторон не существует");
+                    }
+                    else a = value;
+                }
+                else throw new ArgumentException("Треугольник с данными длинами сторон не существует");
+            }    
+        }
+
+        public double B {
+            get => b;
+            set
+            {
+                if (value > 0)
+                {
+                    if (a != 0 && c != 0 && !isTriangleExist(a, value, c))
+                    {
+                        throw new ArgumentException("Треугольник с данными длинами сторон не существует");
+                    }
+                    else b = value;
+                }
+                else throw new ArgumentException("Треугольник с данными длинами сторон не существует");
+            }
+        }
+        public double C {
+            get => c;
+            set
+            {
+                if (value > 0)
+                {
+                    if (b != 0 && a != 0 && !isTriangleExist(a, b, value))
+                    {
+                        throw new ArgumentException("Треугольник с данными длинами сторон не существует");
+                    }
+                    else c = value;
+                }
+                else throw new ArgumentException("Треугольник с данными длинами сторон не существует");
+            }
+        }
+
+        public double Area()
+        {
+            double p = Perimetr() / 2;
+            return Sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+
+        public double Perimetr()=>a + b + c;
+
+        private bool isTriangleExist(double a,double b, double c)=>a + c > b && a + b > c && b + c > a; //проверка на существование треугольника
+
+    }
+}
