a90618f9d38d869dc724cd2c6e66d8e2cc0e819b
Anna Murunova <asmurunova@gmail.com>
2019-07-29 15:10:16 +0400

    Задачи 4.4 и 4.5

---
+++ DelegatesAndExtensions/Task44/Task44/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task44
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[5] { 1, 2, 3, 4, 5 };
+            double[] arr2 = new double[5] { 1.2, 2.8, 3.7, 4.3, 5 };
+            Console.WriteLine(arr.SumElements());
+            Console.WriteLine(arr2.SumElements());
+            Console.ReadKey();
+        }
+
+    }
+
+   static class SumArray 
+    {
+        public static int SumElements(this int[] array)
+        {
+            int res=array[0];
+            for(int i=1;i<array.Length;i++)
+            {
+                res = array[i];
+            }
+            return res;
+
+        }
+        public static double SumElements(this double[] array)
+        {
+            double res = array[0];
+            for (int i = 1; i < array.Length; i++)
+            {
+                res = array[i];
+            }
+            return res;
+
+        }
+    }
+}
---
+++ DelegatesAndExtensions/Task45/Task45/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task45
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = "100";
+            string str2 = "0";
+            string str3 = "-1";
+            Console.WriteLine($"Is string positive?:{str1.IsPositiveNumber()}");
+            Console.WriteLine($"Is string positive?:{str2.IsPositiveNumber()}");
+            Console.WriteLine($"Is string positive?:{str3.IsPositiveNumber()}");
+            Console.ReadKey();
+        }
+
+    }
+
+    public static class StringisNumber
+    {
+        public static bool IsPositiveNumber(this string str)
+        {
+            return StringToInt(str) > 0;
+        }
+
+        private static int StringToInt(String str)
+        {
+            int i = 0;
+            int num = 0;
+            bool isNeg = false;
+
+            //Check for negative sign; if it there, set the isNeg flag
+            if (str[0] == '-')
+            {
+                isNeg = true;
+                i = 1;
+            }
+
+            while (i < str.Length)
+            {
+                if (char.IsDigit(str[i]))
+                {
+                    num *= 10;
+                    num += str[i++] - '0';//Minus the ASCII code of '0' to get the value of the string[i++].
+                }
+                else
+                {
+                    throw new ArgumentException("String is not a number");
+                }
+
+            }
+
+            if (isNeg)
+                num = -num;
+            return num;
+        }
+    }
+}
