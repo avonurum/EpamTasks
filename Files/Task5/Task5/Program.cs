8037db54b114fdbe63c8a6d7020df0eaf68fde0d
Anna Murunova <asmurunova@gmail.com>
2019-08-05 12:01:18 +0400

    Задача 5

---
+++ Files/Task5/Task5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5
+{
+    class Program
+    {
+        private static bool isAllowedToWrite = true;
+        private static string catalogPath;
+        static void Main(string[] args)
+        {
+            try
+            {
+                if (isAllowedToWrite)
+                {
+                    isAllowedToWrite = false;
+                    Console.Write("Введите путь к каталогу: ");
+                    catalogPath = Console.ReadLine();
+                }
+
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
---
+++ Files/Task5/Task5/Surveillance.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Task5
+{
+    class Surveillance
+    {
+        private static string catalogPath;
+        private static bool isAllowedToWrite;
+        private const string PathForLog = @"C:\temp.xml";
+        private static XmlDocument document;
+        public FileSystemWatcher fileWatcher;
+        private DirectoryInfo dirInfo;
+
+        public Surveillance(string catalogPath)
+        {
+            CatalogPath = catalogPath;
+            Thread.Sleep(1500);
+            OnObserve();
+            if (isAllowedToWrite)
+            {
+                try
+                {
+                    var xmlWriter = new XmlTextWriter(PathForLog, Encoding.UTF8)
+                    {
+                        Formatting = Formatting.Indented,
+                        Indentation = 4
+                    };
+                    xmlWriter.WriteStartDocument();
+                    xmlWriter.WriteStartElement("Observe");
+                    xmlWriter.WriteEndElement();
+                    xmlWriter.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            FileInfo temp = new FileInfo(PathForLog);
+            if (temp.IsReadOnly)
+                temp.IsReadOnly = false;
+
+            document = new XmlDocument();
+            document.Load(PathForLog);
+            Subscribe();
+        }
+
+        private void Subscribe()
+        {
+            if (isAllowedToWrite)//если режим наблюдения
+            {
+                dirInfo = new DirectoryInfo(CatalogPath);
+                fileWatcher = new FileSystemWatcher(dirInfo.FullName);
+            }
+            else //если режим отката
+            {
+                Console.Clear();
+            }
+        }
+
+        private void returnChanges()
+        {
+
+        }
+
+        public static string CatalogPath
+        {
+            get => catalogPath;
+            set
+            {
+                if (!Directory.Exists(value))//проверка на наличие каталога
+                {
+                    throw new DirectoryNotFoundException(value);
+                }
+                else
+                {
+                    catalogPath = value;
+                }
+            }
+        }
+
+        private void OnObserve()
+        {
+            bool ModeIsChosen = false;
+            while (!ModeIsChosen)
+            {
+                Console.Clear();
+                Console.WriteLine("Наблюдать или перейти в режим отката ? y/n");
+                var answer = Console.ReadKey();
+                if (answer.Key == ConsoleKey.Y)
+                {
+                    Console.WriteLine();
+                    isAllowedToWrite = true;
+                    Console.WriteLine("Режим наблюдения");
+                    Thread.Sleep(1500);
+                    Console.Clear();
+                    break;
+                }
+
+                else if (answer.Key == ConsoleKey.N)
+                {
+                    Console.WriteLine();
+                    isAllowedToWrite = false;
+                    Console.WriteLine("Режим отката");
+                    Thread.Sleep(1500);
+                    Console.Clear();
+                    break;
+                }
+                else
+                {
+                    throw new ArgumentException("");
+                }
+            }
+        }
+    }
+}
